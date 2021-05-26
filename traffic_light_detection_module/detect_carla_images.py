c6af6577bdb10ab2e34aad03a94ef0f9ab8db3c1
tomlinsbrave <63791680+nietzsche2meetyou@users.noreply.github.com>
2021-05-26 15:30:39 +0200

    Roba davide

---
+++ traffic_light_detection_module/detect_carla_images.py
@@ -0,0 +1,30 @@
+import sys
+import os
+sys.path.insert(0,'traffic_light_detection_module')
+import cv2
+import json
+from postprocessing import draw_boxes
+from postprocessing import decode_netout
+from predict import predict_with_model_from_file, get_model_from_file,predict_with_model_from_carla
+file_path = 'traffic_light_detection_module/config.json'
+with open(file_path) as config_buffer:
+    config = json.loads(config_buffer.read())
+
+
+def detect_on_carla_image(model,carla_image):
+    netout = predict_with_model_from_carla(config, model, carla_image)
+    plt_image = draw_boxes(carla_image, netout, config['model']['classes'])
+    cv2.imshow('detected_image', plt_image)
+    cv2.waitKey(1)
+    list=[]
+
+    image_h, image_w, _ = carla_image.shape
+    labels=config['model']['classes']
+    for box in netout:
+        if box.xmin > image_w or box.xmax > image_w or box.ymin > image_h or box.ymax > image_h:
+            continue
+        label = labels[box.get_label()]
+        score=round(box.get_score(),4)
+        list.append([label,score])
+
+    print(list)
---
+++ traffic_light_detection_module/predict.py
@@ -0,0 +1,58 @@
+#from keras.models import load_model
+from keras.models import load_model
+import os
+import numpy as np
+import yolo
+from yolo import YOLO, dummy_loss
+from preprocessing import load_image_predict,load_image_predict_with_carla
+from postprocessing import decode_netout
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_model(config):
+    model = YOLO(
+         config =config
+    )
+    #model.load_weights(os.path.join(BASE_DIR, config['model']['saved_model_name']))
+    print("Loading Weights")
+    model.model.load_weights("traffic_light_detection_module/traffic-light-detection.h5")
+    print("Model created")
+    return model
+
+
+def get_model_from_file(config):
+    path = os.path.join(BASE_DIR, 'checkpoints', config['model']['saved_model_name'])
+    # model = load_model(path, custom_objects={'custom_loss': dummy_loss})
+    model = load_model(path)
+    
+    return model
+
+
+def predict_with_model_from_file(config, model, image_path):
+    image=load_image_predict(image_path,config['model']['image_h'], config['model']['image_w'])
+
+    dummy_array = np.zeros((1, 1, 1, 1, config['model']['max_obj'], 4))
+    '''
+    netout = model.predict([image, dummy_array])[0]
+    boxes = decode_netout(netout=netout, anchors=config['model']['anchors'],
+                          nb_class=config['model']['num_classes'],
+                          obj_threshold=config['model']['obj_thresh'],
+                          nms_threshold=config['model']['nms_thresh'])
+    '''
+    return model.predict([image, dummy_array])
+
+
+def predict_with_model_from_carla(config, model, image_path):
+    image=load_image_predict_with_carla(image_path,config['model']['image_h'], config['model']['image_w'])
+
+    dummy_array = np.zeros((1, 1, 1, 1, config['model']['max_obj'], 4))
+    '''
+    netout = model.predict([image, dummy_array])[0]
+    boxes = decode_netout(netout=netout, anchors=config['model']['anchors'],
+                          nb_class=config['model']['num_classes'],
+                          obj_threshold=config['model']['obj_thresh'],
+                          nms_threshold=config['model']['nms_thresh'])
+    '''
+    return model.predict([image, dummy_array])
