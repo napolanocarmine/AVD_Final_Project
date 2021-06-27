f16ecf1060ce9debf56169af9b3ebd67f360a8c5
DavideCoppola97 <d.coppola25@studenti.unisa.it>
2021-06-27 16:08:26 +0200

    Revert "Leggi descrizione"
    This reverts commit e1f98e7515549465e296dee24a66034747acc842.

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ COLLISION_RADIUS = 30
 ###############################################################################
 # ACTIVE OR DISACTIVE EVERY SINGLE FUNCTIONS OF OUR PROJECT
 ###############################################################################
-TRACK_TRAFFIC_LIGHT = False
+TRACK_TRAFFIC_LIGHT = True
 FOLLOW_LEAD_VEHICLE = True
 OBSTACLE_AVOIDANCE =  True
 
@@ -956,9 +956,6 @@ def exec_waypoint_nav_demo(args):
                                                     lead_car_pos =[[location.x,location.y]]
                                                     lead_car_length = [dimension.x]
                                                     lead_car_speed = [speed]
-                                    elif abs(yaw-new_ego_state)<=150:
-                                        obstacles.append(obstacle_to_world(location, dimension, orientation))
-                                        obstacles_type.append('vehicle_moving')
                                     else:
                                         obstacles.append(obstacle_to_world(location, dimension, orientation))
                                         obstacles_type.append('vehicle')
